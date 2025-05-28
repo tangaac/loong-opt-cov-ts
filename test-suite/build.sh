a98c7f930c3a6a19e50f30bf84730cfae7677fd2
tangaac <tangyan01@loongson.cn>
2025-05-28 15:21:13 +0800

    update submodule with upstream

--- test-suite/build.sh
+++ test-suite/build.sh
@@ -12,7 +12,7 @@ BIN_DIR=$BUILD_LLVM_DIR/bin
 mkdir -p $BUILD_TEST_SUITE_DIR
 
 cd $BUILD_TEST_SUITE_DIR
-cmake $LLVM_TEST_SUITE_DIR -Wno-dev -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$BIN_DIR/clang -DCMAKE_CXX_COMPILER=$/BIN_DIR/clang++ -G Ninja \
+cmake $LLVM_TEST_SUITE_DIR -Wno-dev -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$BIN_DIR/clang -DCMAKE_CXX_COMPILER=$BIN_DIR/clang++ -G Ninja \
 	-DCMAKE_C_FLAGS_RELEASE="$FLAG -fuse-ld=mold -save-temps=obj -w -Wno-unused-command-line-argument -DNDEBUG -O3 -Qn -Wno-builtin-macro-redefined"  \
 	-DCMAKE_CXX_FLAGS_RELEASE="$FLAG -fuse-ld=mold -save-temps=obj -w -Wno-unused-command-line-argument -DNDEBUG -O3 -Qn -Wno-builtin-macro-redefined"
 ninja
